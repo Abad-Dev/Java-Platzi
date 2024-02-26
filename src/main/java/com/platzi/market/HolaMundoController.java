c0a72d2d652a286c3aa0031301d92011afc6a8f8
Fabian Abad <89323601+Abad-Dev@users.noreply.github.com>
2024-02-25 23:46:36 -0500

    Update HolaMundoController.java

--- src/main/java/com/platzi/market/HolaMundoController.java
+++ src/main/java/com/platzi/market/HolaMundoController.java
@@ -1,9 +1,11 @@
 package com.platzi.market;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/holamundo")
 public class HolaMundoController {
     @GetMapping("/saludo")
     public String Greetings() {
